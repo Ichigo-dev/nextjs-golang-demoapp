235e425d40a32d7ecc801ba96dd14ffb0d58a01d
ichigo-dev <ichigo@web-freework.com>
2021-09-13 21:27:49 +0900

    Exec mkdir src

---
+++ src/pages/contents/index.tsx
@@ -0,0 +1,41 @@
+import { NextPage } from "next";
+import { PureComponent } from "react";
+
+const Contents: NextPage<{ contents: any }> = (props) => {
+  const contents = props.contents;
+  return (
+    <div>
+      {contents.map((c: { name: string; text: string }) => {
+        return (
+          <div>
+            <h3>{c.name}</h3>
+            <p>{c.text}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Contents;
+
+export const getStaticProps = () => {
+  return {
+    props: {
+      contents: [
+        {
+          name: "Hoge",
+          text: "this is hoge.",
+        },
+        {
+          name: "Fuga",
+          text: "this is fuga.",
+        },
+        {
+          name: "Hoge",
+          text: "this is hoge.",
+        },
+      ],
+    },
+  };
+};
